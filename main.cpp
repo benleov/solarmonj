30f688a989b2a7905151fab1330d9b08b27d38fb
Benjamin Leov <benleov@gmail.com>
2016-05-20 18:51:46 +1000

    * Moved ini to seperate folder * Allow --flush-cache parameter to force flushing cache immediately

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 
-#include "INIReader.h"
+#include "ini/INIReader.h"
 
 #include "clientsocket.h"
 #include "socketexception.h"
@@ -41,9 +41,73 @@ static string buildLog(Jfy::InverterData *data) {
 
 }
 
-int main(int argc, char **argv) {
-    // Parse configuration from ini file
+int flushCache(ClientSocket logstash_socket) {
 
+    // Log has been sent; attempt to flush through contents of cache file
+
+    string line;
+
+    ifstream cacheFile(JFY_CACHE_FILENAME.c_str());
+
+    int linesSent = 0;
+
+    if (cacheFile.is_open()) {
+
+
+
+        try {
+
+            while (getline(cacheFile, line)) {
+                logstash_socket << line << "\n";
+                linesSent++;
+            }
+
+            // All data has been sent; remove the cache file
+            std::remove(JFY_CACHE_FILENAME.c_str());
+
+            cout << "All lines sent from cache. Total lines: " << linesSent << endl;
+        }
+        catch (SocketException &e) {
+            // A socket exception occurred while processing the cached data.
+            // Remove the lines that were sent successfully by copying remaining lines to a temp file,
+            // removing the old file, and renaming temp to original
+
+            ofstream tempFile;
+            tempFile.open(JFY_CACHE_TEMP_FILENAME.c_str(), ios::out); // open temp file
+            tempFile << line << endl;   // write current line
+
+            int linesMoved = 1;
+
+            // write remaining lines
+            while (getline(cacheFile, line)) {
+                tempFile << line << "\n";
+                linesMoved++;
+            }
+
+            // close temp file
+            tempFile.close();
+
+            // remove old cache file
+            std::remove(JFY_CACHE_FILENAME.c_str());
+
+            // rename temp to cache file
+            std::rename(JFY_CACHE_TEMP_FILENAME.c_str(), JFY_CACHE_FILENAME.c_str());
+            cout << "Error occurred resending cache. Total sent:  " << linesSent << "Lines remaining: " <<
+            linesMoved << "\n";
+        }
+
+        cacheFile.close();
+    }
+
+    return linesSent;
+
+}
+
+
+
+int main(int argc, char **argv) {
+
+    // Parse configuration from ini file
     INIReader reader(INI_CONFIG_PATH);
 
     if (reader.ParseError() < 0) {
@@ -57,6 +121,30 @@ int main(int argc, char **argv) {
 
     cout << "Using device: " << jfyDevice << endl;
     cout << "Sending to logstash: " << logstashHost << ":" << logstashPort << endl;
+
+    // check if flush is specified
+    if (argc > 1) {
+        string argument = argv[1];
+
+        if(argument.compare("--flush-cache") == 0) {
+            try {
+
+                cout << "Flushing cache manually. " << endl;
+                ClientSocket logstash_socket(logstashHost, logstashPort);
+
+                int linesSent = flushCache(logstash_socket);
+                cout << "Line(s) sent: " << linesSent << endl;
+                return 0;
+            } catch(SocketException &e) {
+                cout << "Could not connect to logstash." << endl;
+               return 1;
+            }
+        } else {
+            cout << "Unknown parameter(s) specified."  << endl;
+            return 1;
+        }
+    }
+
 
     Jfy::Connection conn(jfyDevice);
 
@@ -79,67 +167,16 @@ int main(int argc, char **argv) {
 
     try {
         // Write to logstash
-        ClientSocket client_socket(logstashHost, logstashPort);
-        client_socket << log;
+        ClientSocket logstash_socket(logstashHost, logstashPort);
+        logstash_socket << log;
 
-        // Log has been sent; attempt to flush through contents of cache file
-
-        string line;
-
-        ifstream cacheFile(JFY_CACHE_FILENAME.c_str());
-
-        if (cacheFile.is_open()) {
-            int linesSent = 0;
-
-            try {
-
-                while (getline(cacheFile, line)) {
-                    client_socket << line << "\n";
-                    linesSent++;
-                }
-
-                // All data has been sent; remove the cache file
-                std::remove(JFY_CACHE_FILENAME.c_str());
-
-                cout << "All lines sent from cache. Total lines: " << linesSent << endl;
-            }
-            catch (SocketException &e) {
-                // A socket exception occurred while processing the cached data.
-                // Remove the lines that were sent successfully by copying remaining lines to a temp file,
-                // removing the old file, and renaming temp to original
-
-                ofstream tempFile;
-                tempFile.open(JFY_CACHE_TEMP_FILENAME.c_str(), ios::out); // open temp file
-                tempFile << line << endl;   // write current line
-
-                int linesMoved = 1;
-
-                // write remaining lines
-                while (getline(cacheFile, line)) {
-                    tempFile << line << "\n";
-                    linesMoved++;
-                }
-
-                // close temp file
-                tempFile.close();
-
-                // remove old cache file
-                std::remove(JFY_CACHE_FILENAME.c_str());
-
-                // rename temp to cache file
-                std::rename(JFY_CACHE_TEMP_FILENAME.c_str(), JFY_CACHE_FILENAME.c_str());
-                cout << "Error occurred resending cache. Total sent:  " << linesSent << "Lines remaining: " <<
-                linesMoved << "\n";
-            }
-
-
-            cacheFile.close();
-        }
+        // Log has been sent successfully; attempt to flush cache
+        flushCache(logstash_socket);
     }
     catch (SocketException &e) {
         std::cout << "Error writing to logging server:\n\"" << e.description() << "\". Writing log cache file.\n";
 
-        // Write to cache
+        // Write log to cache
         ofstream cacheFile;
         cacheFile.open(JFY_CACHE_FILENAME.c_str(), ios::out | ios::app);
         cacheFile << log;
